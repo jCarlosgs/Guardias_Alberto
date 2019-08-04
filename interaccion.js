48967584d9df88ac1442d392f523011054c57391
jCarlosgs <49098351+jCarlosgs@users.noreply.github.com>
2019-08-04 15:42:13 -0500

    Add files via upload

--- interaccion.js
+++ interaccion.js
@@ -1,5 +1,25 @@
 document.getElementById('facebook').addEventListener('click',boton);
-
 function boton() {
   window.location='https://www.facebook.com/ENFESP-enfermer%C3%ADa-especializada-244417149538647/?epa=SEARCH_BOX';
+}
+
+document.getElementById('Nosotros').addEventListener('click',menu);
+function menu() {
+  window.location='pagina_2.html'
+}
+
+
+document.getElementById('Servicios').addEventListener('click',servicios);
+function servicios() {
+  window.location='pagina_3.html'
+}
+
+document.getElementById('Precios').addEventListener('click',precios);
+function precios() {
+  window.location='pagina_4.html'
+}
+
+document.getElementById('Contactos').addEventListener('click',contactos);
+function contactos() {
+  window.location='pagina_5.html'
 }
